3417bd432b3a1f462868b1ea4382ab2529da068e
SevenStep_Lab <mmik012@126.com>
2020-07-20 17:48:18 +0800

    继续修改

--- pages/page_information/page_information.js
+++ pages/page_information/page_information.js
@@ -5,13 +5,20 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    precision_array: ['高','中','低'],
+    objectprecision_array: [{id: 0x01, name: '高'},
+                            {id: 0x02, name: '中'},
+                            {id: 0x03, name: '低'}],
+    precision_index: 1,           
+    version_number: ' v0.2.0',//版本号
+    showView: false,//developer显示状态
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    showView: (options.showView == "true" ? true : false)
 
   },
 
@@ -65,6 +72,24 @@ Page({
       title: 'BLE温湿度检测',
       path: '../main_page/main_page',
     }
+  },
+
+
+
+  bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      precision_index: e.detail.value
+    })
+  },
+
+  /**
+   * 显示函数--developer是否显示
+   */
+  onChangeShowState() {
+    this.setData({
+      showView: (!this.data.showView),
+    })
   },
   
 })
